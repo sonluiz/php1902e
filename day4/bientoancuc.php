724a274383a8a448fcf3f36e2b519145c4d9eb47
hocphp <sonhong121997@gmail.com>
2019-05-29 20:24:39 +0700

    bien toan cuc

---
+++ day4/bientoancuc.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>sonhong </title>
+</head>
+<body>
+<h1>Phạm vi của biến</h1>
+<pre>
+    - Biến cục bộ : chỉ có ảnh hưởng và chỉ sử dụng được trong 1 không gian nhất định
+    - Biến toàn cục : có ảnh hưởng và có thể sử dụng ở mọi nơi
+    - Nếu tên biến bị trùng thì nếu sử dụng ngoài hàm thì php sẽ lấy giá trị của biến
+    toàn cục
+    ***
+    Tham số khi khai báo hàm là biến cục bộ và chỉ có tác dụng trong hàm
+    ví dụ như là biến $r là biến cục bộ
+</pre>
+</body>
+</html>
